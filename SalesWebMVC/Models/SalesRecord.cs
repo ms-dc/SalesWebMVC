5c0b9434ed03b3628aa013d6648906f1a2c5a510
xReiketsu <matheus.santana.da.costa@gmail.com>
2020-08-23 11:50:29 -0300

    Seeding Service

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -16,9 +16,8 @@ namespace SalesWebMVC.Models
 
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
-        {
-            Id = id;
+        public SalesRecord(DateTime date, double amount, SaleStatus status, Seller seller)
+        { 
             Date = date;
             Amount = amount;
             Status = status;
